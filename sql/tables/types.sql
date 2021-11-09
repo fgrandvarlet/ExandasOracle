9be408db945648b8f6d25680611483be0e12eaad
François Grandvarlet <92960730+fgrandvarlet@users.noreply.github.com>
2021-11-09 16:29:33 +0100

    Add files via upload

--- sql/tables/types.sql
+++ sql/tables/types.sql
@@ -1,14 +1,36 @@
 
--- A COMPLETER DANS UN DEUXIEME LOT
-
 CREATE TABLE src_types
 (
     type_name varchar(128) not null,
+	typecode varchar(128),
+	attributes integer,
+	methods integer,
+	predefined varchar(3),
+	incomplete varchar(3),
+	final varchar(3),
+	instantiable varchar(3),
+	persistable varchar(3),
+	supertype_owner varchar(128),
+	supertype_name varchar(128),
+	local_attributes integer,
+	local_methods integer,
     CONSTRAINT pk_src_types PRIMARY KEY (type_name)
 );
 
 CREATE TABLE tgt_types
 (
     type_name varchar(128) not null,
+	typecode varchar(128),
+	attributes integer,
+	methods integer,
+	predefined varchar(3),
+	incomplete varchar(3),
+	final varchar(3),
+	instantiable varchar(3),
+	persistable varchar(3),
+	supertype_owner varchar(128),
+	supertype_name varchar(128),
+	local_attributes integer,
+	local_methods integer,
     CONSTRAINT pk_tgt_types PRIMARY KEY (type_name)
 );
