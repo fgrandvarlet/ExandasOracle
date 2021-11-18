5ea6d7890bcffad8343e94db676950023561c381
François Grandvarlet <92960730+fgrandvarlet@users.noreply.github.com>
2021-11-18 16:21:51 +0100

    Add files via upload

---
+++ ExandasOracle/Domain/Synonym.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+using ExandasOracle.Properties;
+
+namespace ExandasOracle.Domain
+{
+    public class Synonym
+    {
+        const string ENTITY = "SYNONYM";
+        public string Owner { get; set; }
+        public string SynonymName { get; set; }
+        public string TableOwner { get; set; }
+        public string TableName { get; set; }
+        public string DbLink { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="comparisonSet"></param>
+        /// <param name="list"></param>
+        public void Compare(Synonym target, ComparisonSet comparisonSet, List<DeltaReport> list)
+        {
+            var parentObject = string.Format("owner={0}", this.Owner);
+            
+            if (this.TableOwner != target.TableOwner && comparisonSet.Schema1 == comparisonSet.Schema2)
+            {
+                list.Add(new DeltaReport(
+                    comparisonSet.Uid, ENTITY, this.SynonymName, parentObject, Strings.PropertyDifference, "TABLE_OWNER", this.TableOwner, target.TableOwner
+                    ));
+            }
+            if (this.TableName != target.TableName)
+            {
+                list.Add(new DeltaReport(
+                    comparisonSet.Uid, ENTITY, this.SynonymName, parentObject, Strings.PropertyDifference, "TABLE_NAME", this.TableName, target.TableName
+                    ));
+            }
+            if (this.DbLink != target.DbLink)
+            {
+                list.Add(new DeltaReport(
+                    comparisonSet.Uid, ENTITY, this.SynonymName, parentObject, Strings.PropertyDifference, "DB_LINK", this.DbLink, target.DbLink
+                    ));
+            }
+        }
+
+    }
+}
