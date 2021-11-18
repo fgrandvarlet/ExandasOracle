a44911119a9d093bec5769fded325060a87c5e71
François Grandvarlet <92960730+fgrandvarlet@users.noreply.github.com>
2021-11-18 14:08:05 +0100

    Update todo.sql

--- sql/todo.sql
+++ sql/todo.sql
@@ -2,3 +2,4 @@ agrandir la zone password de la table connection_params	-->> OK
 
 limiter les zones de saisie de l'ihm selon les tailles des colonnes des tables sous-jacentes
 
+ajouter en objet parent table_name pour l'entité IndexExpression
