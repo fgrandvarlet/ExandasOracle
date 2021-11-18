ebc172a854c8d7842dcced753f27bdd22e8484ed
François Grandvarlet <92960730+fgrandvarlet@users.noreply.github.com>
2021-11-18 14:02:22 +0100

    Add files via upload

---
+++ ExandasOracle/Domain/IndexExpression.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+using ExandasOracle.Properties;
+
+namespace ExandasOracle.Domain
+{
+    public class IndexExpression
+    {
+        const string ENTITY = "INDEX EXPRESSION";
+        public string IndexName { get; set; }
+        public string TableOwner { get; set; }
+        public string TableName { get; set; }
+        public string ColumnExpression { get; set; }
+        public decimal ColumnPosition { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="comparisonSet"></param>
+        /// <param name="list"></param>
+        public void Compare(IndexExpression target, ComparisonSet comparisonSet, List<DeltaReport> list)
+        {
+            var objectValue = string.Format("{0}#{1}", this.IndexName, this.ColumnPosition); 
+            
+            if (this.TableOwner != target.TableOwner && comparisonSet.Schema1 == comparisonSet.Schema2)
+            {
+                list.Add(new DeltaReport(
+                    comparisonSet.Uid, ENTITY, objectValue, null, Strings.PropertyDifference, "TABLE_OWNER", this.TableOwner, target.TableOwner
+                    ));
+            }
+            if (this.TableName != target.TableName)
+            {
+                list.Add(new DeltaReport(
+                    comparisonSet.Uid, ENTITY, objectValue, null, Strings.PropertyDifference, "TABLE_NAME", this.TableName, target.TableName
+                    ));
+            }
+            if (this.ColumnExpression != target.ColumnExpression)
+            {
+                list.Add(new DeltaReport(
+                    comparisonSet.Uid, ENTITY, objectValue, null, Strings.PropertyDifference, "COLUMN_EXPRESSION", this.ColumnExpression, target.ColumnExpression
+                    ));
+            }
+        }
+
+    }
+}
