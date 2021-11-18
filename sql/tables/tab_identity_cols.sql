fb4e0cca900c21e03d1706f879ff13cc7d988217
François Grandvarlet <92960730+fgrandvarlet@users.noreply.github.com>
2021-11-18 09:10:35 +0100

    Update tab_identity_cols.sql

--- sql/tables/tab_identity_cols.sql
+++ sql/tables/tab_identity_cols.sql
@@ -1,16 +1,20 @@
 
--- A COMPLETER DANS UN SECOND LOT
-
 CREATE TABLE src_tab_identity_cols
 (
     table_name varchar(128) not null,
-    tablespace_name varchar(30),
-    CONSTRAINT pk_src_tab_identity_cols PRIMARY KEY (table_name)
+    column_name varchar(128) not null,
+    generation_type varchar(10),
+    sequence_name varchar(128) not null,
+    identity_options varchar(298),
+    CONSTRAINT pk_src_tab_identity_cols PRIMARY KEY (table_name, column_name)
 );
 
 CREATE TABLE tgt_tab_identity_cols
 (
     table_name varchar(128) not null,
-    tablespace_name varchar(30),
-    CONSTRAINT pk_tgt_tab_identity_cols PRIMARY KEY (table_name)
+    column_name varchar(128) not null,
+    generation_type varchar(10),
+    sequence_name varchar(128) not null,
+    identity_options varchar(298),
+    CONSTRAINT pk_tgt_tab_identity_cols PRIMARY KEY (table_name, column_name)
 );
